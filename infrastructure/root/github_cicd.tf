1f9c3dd7e5a3dd20c51474a8fededadc2fc13e13
Edward Gorman <14982444+edgorman@users.noreply.github.com>
2026-02-10 16:41:45 +0000

    Feat/GitHub cicd (#3)
    * feat(infra): Enable service, add github cicd config
    
    * feat(cicd): Add github cicd for infrastructure
    
    * fix(cicd): Pin terraform versions
    
    * feat(infra): Remove example config, Add tfvars/tfbackend files
    
    * feat(infra): Update iam permissions for github cicd
    
    * feat(cicd): Use gcp projects relevant envs for auth/secrets
    
    * feat(infra): Add github variables for github cicd
    
    * feat(cicd): Use github variables to inject gcp auth info

---
+++ infrastructure/root/github_cicd.tf
@@ -0,0 +1,55 @@
+resource "google_service_account" "github_actions" {
+  project      = var.gcp_provider_project_id
+  account_id   = "github-actions"
+  display_name = "GitHub Actions Service Account"
+}
+
+resource "google_iam_workload_identity_pool" "github_pool" {
+  project                   = var.gcp_provider_project_id
+  workload_identity_pool_id = "github-pool"
+  display_name              = "GitHub Pool"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_provider" {
+  project                            = var.gcp_provider_project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+  display_name                       = "GitHub Provider"
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+    "attribute.actor"      = "assertion.actor"
+  }
+
+  attribute_condition = "assertion.repository == '${var.github_repository_owner}/${var.github_repository_name}'"
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_service_account_iam_member" "github_actions_wic" {
+  service_account_id = google_service_account.github_actions.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/edgorman/${var.github_repository_name}"
+}
+
+resource "google_project_iam_member" "github_actions_admin" {
+  for_each = local.all_projects
+  project  = each.value.project_id
+  role     = "roles/editor"
+  member   = "serviceAccount:${google_service_account.github_actions.email}"
+}
+
+resource "github_actions_variable" "workload_identity_provider" {
+  repository    = github_repository.repository.name
+  variable_name = "WORKLOAD_IDENTITY_PROVIDER"
+  value         = google_iam_workload_identity_pool_provider.github_provider.name
+}
+
+resource "github_actions_variable" "service_account" {
+  repository    = github_repository.repository.name
+  variable_name = "SERVICE_ACCOUNT"
+  value         = google_service_account.github_actions.email
+}
