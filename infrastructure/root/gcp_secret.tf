3aa0df0086240ba6810a89c7d365cb3d163c7ac7
Edward Gorman <ejgorman@gmail.com>
2026-02-14 20:04:14 +0000

    fix(infra): Remove unnecessary files, update sa iam

--- infrastructure/root/gcp_secret.tf
+++ infrastructure/root/gcp_secret.tf
@@ -11,10 +11,3 @@ resource "google_secret_manager_secret_version" "github_provider_token_v1" {
   secret      = google_secret_manager_secret.github_provider_token.id
   secret_data = var.github_provider_token
 }
-
-resource "google_secret_manager_secret_iam_member" "github_actions_secret_accessor" {
-  project   = var.gcp_provider_project_id
-  secret_id = google_secret_manager_secret.github_provider_token.secret_id
-  role      = "roles/secretmanager.secretAccessor"
-  member    = "serviceAccount:${google_service_account.github_actions.email}"
-}
